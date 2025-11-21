a78f3540c5a5486c1f5e728ec68cc9b222ed0dec
Darren Fobissie Blese <dfobissieblese@gmail.com>
2025-11-21 07:55:52 -0500

    commit 3, submit.

--- lib/views/home_view.dart
+++ lib/views/home_view.dart
@@ -1,5 +1,7 @@
-import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
+import "package:finance_app/widgets/custom_app_bar.dart";
+import "package:finance_app/widgets/total_value.dart";
+import "package:flutter/material.dart";
+import "package:flutter/services.dart";
 import 'package:iconsax/iconsax.dart';
 
 class HomeScreen extends StatelessWidget {
@@ -9,98 +11,35 @@ class HomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return AnnotatedRegion<SystemUiOverlayStyle>(
       value: SystemUiOverlayStyle.light.copyWith(
-        statusBarColor: const Color(0xFF1A1A1A),
-        systemNavigationBarColor: const Color(0xFF1A1A1A),
+        statusBarColor: Color(0xFF1A1A1A),
+        systemNavigationBarColor: Color(0xFF1E1E1E),
         systemNavigationBarIconBrightness: Brightness.light,
       ),
-      child: SafeArea( // moved SafeArea to wrap Scaffold
-        left: true,
-        top: true,
-        right: true,
-        bottom: false,
-        child: Stack(
-          children: [
-            Scaffold(
-              backgroundColor: const Color(0xFF1A1A1A),
-              appBar: AppBar(
-                backgroundColor: const Color(0xFF1A1A1A),
-                elevation: 0,
-                toolbarHeight: 80,
-                automaticallyImplyLeading: false,
-                titleSpacing: 0,
-                title: Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 16.0),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Container(
-                        width: 44,
-                        height: 44,
-                        decoration: BoxDecoration(
-                          shape: BoxShape.circle,
-                          color: const Color(0xFF2A2A2A),
-                          border: Border.all(
-                            color: const Color(0xFF3A3A3A),
-                            width: 1,
-                          ),
-                        ),
-                        child: ClipOval(
-                          child: Image.asset(
-                            'assets/images/profile_picture.png',
-                            fit: BoxFit.cover,
-                          ),
-                        ),
-                      ),
-                      Row(
+      child: Stack(
+        children: [
+          Scaffold(
+            backgroundColor: Color(0xFF1A1A1A),
+            body: SafeArea(
+              child: Column(
+                children: [
+                  const CustomAppBar(),
+                  Expanded(
+                    child: SingleChildScrollView(
+                      padding: EdgeInsets.symmetric(horizontal: 20),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
                         children: [
-                          IconButton(
-                            onPressed: () {},
-                            icon: const Icon(Iconsax.user, color: Colors.white, size: 22),
-                            padding: const EdgeInsets.all(8),
-                            constraints: const BoxConstraints(),
-                          ),
-                          const SizedBox(width: 12),
-                          IconButton(
-                            onPressed: () {},
-                            icon: const Icon(Iconsax.notification, color: Colors.white, size: 22),
-                            padding: const EdgeInsets.all(8),
-                            constraints: const BoxConstraints(),
-                          ),
+                          SizedBox(height: 20),
+                          const TotalValueSection(),
                         ],
                       ),
-                    ],
-                  ),
-                ),
-              ),
-              body: Column(
-                children: [
-                  Row(
-                    children: [
-                      Container(
-                        width: 44,
-                        height: 44,
-                        decoration: BoxDecoration(
-                          shape: BoxShape.circle,
-                          color: const Color(0xFF2A2A2A),
-                          border: Border.all(
-                            color: const Color(0xFF3A3A3A),
-                            width: 1,
-                          ),
-                        ),
-                        child: ClipOval(
-                          child: Image.asset(
-                            'assets/images/profile_picture.png',
-                            fit: BoxFit.cover,
-                          ),
-                        ),
-                      ),
-                    ],
+                    ),
                   ),
                 ],
               ),
             ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
